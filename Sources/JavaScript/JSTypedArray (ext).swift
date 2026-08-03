886225d0fb388428ae9368a35711d2717ba29495
Diana Ma <diana@rarestype.com>
2026-08-03 22:09:56 +0000

    feedback

--- Sources/JavaScript/JSTypedArray (ext).swift
+++ Sources/JavaScript/JSTypedArray (ext).swift
@@ -2,9 +2,9 @@
 import JavaScriptBackend
 
 extension JSTypedArray<UInt8> {
-    public convenience init?(object: JSObject) {
+    public convenience init(object: JSObject) {
         let Uint8Array: JSObject = JavaScriptClass.Uint8Array.constructor
-        if  object.isInstanceOf(Uint8Array) {
+        if  object.isInstanceOf(Uint8Array) || object.is(.Uint8ClampedArray) {
             self.init(unsafelyWrapping: object)
         } else {
             // Wrap ArrayBuffers, JS Arrays, or other TypedArray views into a Uint8Array
